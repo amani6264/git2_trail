0aefa518c9e1d747a522b107b5aed2761c50ca1f
farooqui ziauddin <f.ziyauddin@gmail.com>
2023-01-08 00:11:38 +0530

    I fix this problem you can check and pull

--- prime.js
+++ prime.js
@@ -1,8 +1,15 @@
 let number=5;
+
 let factors=0;
+
 for(let i=1;i<=5;i++){
-if(i%5==0){
+
+if(number%i==0){
+
 factors++;
+
 }
+
 }
+
 (factors==2)? console.log("prime number") : console.log ("not a prime number")
